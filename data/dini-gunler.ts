9ea2e8036da2bee9c721bcf58063b2d39fe6f78d
Ahmet Nebioğlu <info@ahmetnebioglu.com.tr>
2024-09-18 14:23:15 +0300

    2027 ve 2028 Dini günler güncellendi.

--- data/dini-gunler.ts
+++ data/dini-gunler.ts
@@ -1,141 +1,234 @@
 export const ozelGunler_2024 = [
-    {
-        title: 'REGAİB KANDİLİ',
-        date: '11 Ocak'
-    },
-    {
-        title: 'ÜÇ AYLARIN BAŞLANGICI',
-        date: '12 Ocak'
-    },
-    {
-        title: 'MİRAC KANDİLİ',
-        date: '6 Şubat'
-    },
-    {
-        title: 'BERAT KANDİLİ',
-        date: '24 Şubat'
-    },
-    {
-        title: 'RAMAZAN BAŞLANGICI',
-        date: '11 Mart'
-    },
-    {
-        title: 'KADİR GECESİ',
-        date: '5 Nisan'
-    },
-    {
-        title: 'RAMAZAN BAYRAMI',
-        date: '10 Nisan'
-    },
-    {
-        title: 'KURBAN BAYRAMI (1.Gün)',
-        date: '16 Haziran'
-    },
-    {
-        title: 'HİCRİ YILBAŞI',
-        date: '7 Temmuz'
-    },
-    {
-        title: 'AŞURE GÜNÜ',
-        date: '16 Temmuz'
-    },
-    {
-        title: 'MEVLİD KANDİLİ',
-        date: '14 Eylül'
-    }
+  {
+    title: "REGAİB KANDİLİ",
+    date: "11 Ocak",
+  },
+  {
+    title: "ÜÇ AYLARIN BAŞLANGICI",
+    date: "12 Ocak",
+  },
+  {
+    title: "MİRAC KANDİLİ",
+    date: "6 Şubat",
+  },
+  {
+    title: "BERAT KANDİLİ",
+    date: "24 Şubat",
+  },
+  {
+    title: "RAMAZAN BAŞLANGICI",
+    date: "11 Mart",
+  },
+  {
+    title: "KADİR GECESİ",
+    date: "5 Nisan",
+  },
+  {
+    title: "RAMAZAN BAYRAMI",
+    date: "10 Nisan",
+  },
+  {
+    title: "KURBAN BAYRAMI (1.Gün)",
+    date: "16 Haziran",
+  },
+  {
+    title: "HİCRİ YILBAŞI",
+    date: "7 Temmuz",
+  },
+  {
+    title: "AŞURE GÜNÜ",
+    date: "16 Temmuz",
+  },
+  {
+    title: "MEVLİD KANDİLİ",
+    date: "14 Eylül",
+  },
 ];
 
 export const ozelGunler_2025 = [
-    {
-        title: 'ÜÇ AYLARIN BAŞLANGICI',
-        date: '1 Ocak'
-    },
-    {
-        title: 'REGAİB KANDİLİ',
-        date: '2 Ocak'
-    },
-    {
-        title: 'MİRAC KANDİLİ',
-        date: '26 Ocak'
-    },
-    {
-        title: 'BERAT KANDİLİ',
-        date: '13 Şubat'
-    },
-    {
-        title: 'RAMAZAN BAŞLANGICI',
-        date: '1 Mart'
-    },
-    {
-        title: 'KADİR GECESİ',
-        date: '26 Mart'
-    },
-    {
-        title: 'RAMAZAN BAYRAMI',
-        date: '30 Mart'
-    },
-    {
-        title: 'KURBAN BAYRAMI',
-        date: '6 Haziran'
-    },
-    {
-        title: 'HİCRİ YILBAŞI',
-        date: '26 Haziran'
-    },
-    {
-        title: 'AŞURE GÜNÜ',
-        date: '5 Temmuz'
-    },
-    {
-        title: 'MEVLİD KANDİLİ',
-        date: '3 Eylül'
-    }
+  {
+    title: "ÜÇ AYLARIN BAŞLANGICI",
+    date: "1 Ocak",
+  },
+  {
+    title: "REGAİB KANDİLİ",
+    date: "2 Ocak",
+  },
+  {
+    title: "MİRAC KANDİLİ",
+    date: "26 Ocak",
+  },
+  {
+    title: "BERAT KANDİLİ",
+    date: "13 Şubat",
+  },
+  {
+    title: "RAMAZAN BAŞLANGICI",
+    date: "1 Mart",
+  },
+  {
+    title: "KADİR GECESİ",
+    date: "26 Mart",
+  },
+  {
+    title: "RAMAZAN BAYRAMI",
+    date: "30 Mart",
+  },
+  {
+    title: "KURBAN BAYRAMI",
+    date: "6 Haziran",
+  },
+  {
+    title: "HİCRİ YILBAŞI",
+    date: "26 Haziran",
+  },
+  {
+    title: "AŞURE GÜNÜ",
+    date: "5 Temmuz",
+  },
+  {
+    title: "MEVLİD KANDİLİ",
+    date: "3 Eylül",
+  },
 ];
 
 export const ozelGunler_2026 = [
+  {
+    title: "MİRAC KANDİLİ",
+    date: "15 Ocak",
+  },
+  {
+    title: "BERAT KANDİLİ",
+    date: "2 Şubat",
+  },
+  {
+    title: "RAMAZAN BAŞLANGICI",
+    date: "19 Şubat",
+  },
+  {
+    title: "KADİR GECESİ",
+    date: "16 Mart",
+  },
+  {
+    title: "RAMAZAN BAYRAMI",
+    date: "20 Mart",
+  },
+  {
+    title: "KURBAN BAYRAMI",
+    date: "27 Mayıs",
+  },
+  {
+    title: "HİCRİ YILBAŞI",
+    date: "16 Haziran",
+  },
+  {
+    title: "AŞURE GÜNÜ",
+    date: "25 Haziran",
+  },
+  {
+    title: "MEVLİD KANDİLİ",
+    date: "24 Ağustos",
+  },
+  {
+    title: "ÜÇ AYLARIN BAŞLANGICI",
+    date: "10 Aralık",
+  },
+  {
+    title: "REGAİB KANDİLİ",
+    date: "10 Aralık",
+  },
+];
 
-    {
-        title: 'MİRAC KANDİLİ',
-        date: '15 Ocak'
-    },
-    {
-        title: 'BERAT KANDİLİ',
-        date: '2 Şubat'
-    },
-    {
-        title: 'RAMAZAN BAŞLANGICI',
-        date: '19 Şubat'
-    },
-    {
-        title: 'KADİR GECESİ',
-        date: '16 Mart'
-    },
-    {
-        title: 'RAMAZAN BAYRAMI',
-        date: '20 Mart'
-    },
-    {
-        title: 'KURBAN BAYRAMI',
-        date: '27 Mayıs'
-    },
-    {
-        title: 'HİCRİ YILBAŞI',
-        date: '16 Haziran'
-    },
-    {
-        title: 'AŞURE GÜNÜ',
-        date: '25 Haziran'
-    },
-    {
-        title: 'MEVLİD KANDİLİ',
-        date: '24 Ağustos'
-    },
-    {
-        title: 'ÜÇ AYLARIN BAŞLANGICI',
-        date: '10 Aralık'
-    },
-    {
-        title: 'REGAİB KANDİLİ',
-        date: '10 Aralık'
-    },
+export const ozelGunler_2027 = [
+  {
+    title: "berat kandili",
+    date: "22 Ocak 2027",
+  },
+  {
+    title: "ramazanın ilk günü",
+    date: "08 Şubat 2027",
+  },
+  {
+    title: "kadir gecesi",
+    date: "05 Mart 2027",
+  },
+  {
+    title: "ramazan bayramı",
+    date: "9 Mart 2027",
+  },
+  {
+    title: "kurban bayramı",
+    date: "16 Mayıs 2027",
+  },
+  {
+    title: "hicri yılbaşı",
+    date: "6 Haziran 2027",
+  },
+  {
+    title: "aşure günü",
+    date: "15 Haziran 2027",
+  },
+  {
+    title: "mevlid kandili",
+    date: "13 Ağustos 2027",
+  },
+  {
+    title: "regaip kandili",
+    date: "02 Aralık 2027",
+  },
+  {
+    title: "üç ayların başlangıcı",
+    date: "29 Kasım 2027",
+  },
+  {
+    title: "miraç kandili",
+    date: "24 Aralık 2027",
+  },
+];
+
+export const ozelGunler_2028 = [
+  {
+    title: "berat kandili",
+    date: "11 Ocak 2028",
+  },
+  {
+    title: "ramazanın ilk günü",
+    date: "28 Ocak 2028",
+  },
+  {
+    title: "kadir gecesi",
+    date: "22 Şubat 2028",
+  },
+  {
+    title: "ramazan bayramı",
+    date: "26 Şubat 2028",
+  },
+  {
+    title: "kurban bayramı",
+    date: "5 Mayıs 2028",
+  },
+  {
+    title: "hicri yılbaşı",
+    date: "25 Mayıs 2028",
+  },
+  {
+    title: "aşure günü",
+    date: "3 Haziran 2028",
+  },
+  {
+    title: "mevlid kandili",
+    date: "2 Ağustos 2028",
+  },
+  {
+    title: "regaip kandili",
+    date: "23 Kasım 2028",
+  },
+  {
+    title: "üç ayların başlangıcı",
+    date: "18 Kasım 2028",
+  },
+  {
+    title: "miraç kandili",
+    date: "13 Aralık 2028",
+  },
 ];
--- src/pages/DiniGunler/DiniGunler.tsx
+++ src/pages/DiniGunler/DiniGunler.tsx
@@ -1,80 +1,193 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import {
-    IonContent,
-    IonItem,
-    IonList,
-    IonLabel, IonGrid, IonCol, IonRow
-} from '@ionic/react';
-import "./DiniGunler.css"
+  IonContent,
+  IonItem,
+  IonList,
+  IonLabel,
+  IonGrid,
+  IonCol,
+  IonRow,
+} from "@ionic/react";
+import "./DiniGunler.css";
 import Header from "../../components/Header";
-import {ozelGunler_2024, ozelGunler_2025, ozelGunler_2026} from "../../../data/dini-gunler";
+import {
+  ozelGunler_2024,
+  ozelGunler_2025,
+  ozelGunler_2026,
+  ozelGunler_2027,
+  ozelGunler_2028,
+} from "../../../data/dini-gunler";
 
 function DiniGunler() {
-    const [activeTab, setActiveTab] = useState("2024");
-    return (
-        <>
-            <Header pageTitle={"Dini Günler"}/>
-            <IonContent class="ion-padding bg-white bg-color-white">
-                <IonGrid>
-                    <IonRow>
-                        <IonCol onClick={() => setActiveTab("2024")}
-                                className={`py-3 ${activeTab == "2024" ? "bg-[#4ac3a4]" : "bg-[#2c7663] "}`}>
-                            <span className={"text-white mx-auto"}>2024</span>
-                        </IonCol>
-                        <IonCol onClick={() => setActiveTab("2025")}
-                                className={`py-3 ${activeTab == "2025" ? "bg-[#4ac3a4]" : "bg-[#2c7663] "}`}>
-                            <span className={"text-white mx-auto"}>2025</span>
-                        </IonCol>
-                        <IonCol onClick={() => setActiveTab("2026")}
-                                className={`py-3 ${activeTab == "2026" ? "bg-[#4ac3a4]" : "bg-[#2c7663] "}`}>
-                            <span className={"text-white mx-auto"}>2026</span>
-                        </IonCol>
-                    </IonRow>
-                </IonGrid>
-                <IonList lines={"none"}>
-                    {activeTab == "2024" && (
-                        ozelGunler_2024.map((gun, index) => (
-                            <IonItem key={`2024-${index}`} className={"mt-3 dg-ion-item"}>
-                                <IonLabel><span className={"font-medium text-sm text-green-800 whitespace-pre-wrap"}>
-                            {gun.title}
-                        </span></IonLabel>
-                                <IonLabel slot={"end"}><span
-                                    className={"font-medium text-sm float-right text-green-800 whitespace-pre-wrap"}>
-                            {gun.date}
-                        </span></IonLabel>
-                            </IonItem>
-                        ))
-                    )}
-                    {activeTab == "2025" && (
-                        ozelGunler_2025.map((gun, index) => (
-                            <IonItem key={`2025-${index}`} className={"mt-3 dg-ion-item"}>
-                                <IonLabel><span className={"font-medium text-sm text-green-800 whitespace-pre-wrap"}>
-                            {gun.title}
-                        </span></IonLabel>
-                                <IonLabel slot={"end"}><span
-                                    className={"font-medium text-sm float-right text-green-800 whitespace-pre-wrap"}>
-                            {gun.date}
-                        </span></IonLabel>
-                            </IonItem>
-                        ))
-                    )}
-                    {activeTab == "2026" && (
-                        ozelGunler_2026.map((gun, index) => (
-                            <IonItem key={`2026-${index}`} className={"mt-3 dg-ion-item"}>
-                                <IonLabel><span className={"font-medium text-sm text-green-800 whitespace-pre-wrap"}>
-                            {gun.title}
-                        </span></IonLabel>
-                                <IonLabel slot={"end"}><span
-                                    className={"font-medium text-sm float-right text-green-800 whitespace-pre-wrap"}>
-                            {gun.date}
-                        </span></IonLabel>
-                            </IonItem>
-                        ))
-                    )}
-                </IonList>
-            </IonContent>
-        </>
-    );
+  const [activeTab, setActiveTab] = useState("2024");
+  return (
+    <>
+      <Header pageTitle={"Dini Günler"} />
+      <IonContent class="ion-padding bg-white bg-color-white">
+        <IonGrid>
+          <IonRow>
+            <IonCol
+              onClick={() => setActiveTab("2024")}
+              className={`py-3 ${
+                activeTab == "2024" ? "bg-[#4ac3a4]" : "bg-[#2c7663] "
+              }`}
+            >
+              <span className={"text-white mx-auto"}>2024</span>
+            </IonCol>
+            <IonCol
+              onClick={() => setActiveTab("2025")}
+              className={`py-3 ${
+                activeTab == "2025" ? "bg-[#4ac3a4]" : "bg-[#2c7663] "
+              }`}
+            >
+              <span className={"text-white mx-auto"}>2025</span>
+            </IonCol>
+            <IonCol
+              onClick={() => setActiveTab("2026")}
+              className={`py-3 ${
+                activeTab == "2026" ? "bg-[#4ac3a4]" : "bg-[#2c7663] "
+              }`}
+            >
+              <span className={"text-white mx-auto"}>2026</span>
+            </IonCol>
+            <IonCol
+              onClick={() => setActiveTab("2027")}
+              className={`py-3 ${
+                activeTab == "2027" ? "bg-[#4ac3a4]" : "bg-[#2c7663] "
+              }`}
+            >
+              <span className={"text-white mx-auto"}>2027</span>
+            </IonCol>
+            <IonCol
+              onClick={() => setActiveTab("2028")}
+              className={`py-3 ${
+                activeTab == "2028" ? "bg-[#4ac3a4]" : "bg-[#2c7663] "
+              }`}
+            >
+              <span className={"text-white mx-auto"}>2028</span>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+        <IonList lines={"none"}>
+          {activeTab == "2024" &&
+            ozelGunler_2024.map((gun, index) => (
+              <IonItem key={`2024-${index}`} className={"mt-3 dg-ion-item"}>
+                <IonLabel>
+                  <span
+                    className={
+                      "font-medium text-sm text-green-800 whitespace-pre-wrap"
+                    }
+                  >
+                    {gun.title}
+                  </span>
+                </IonLabel>
+                <IonLabel slot={"end"}>
+                  <span
+                    className={
+                      "font-medium text-sm float-right text-green-800 whitespace-pre-wrap"
+                    }
+                  >
+                    {gun.date}
+                  </span>
+                </IonLabel>
+              </IonItem>
+            ))}
+          {activeTab == "2025" &&
+            ozelGunler_2025.map((gun, index) => (
+              <IonItem key={`2025-${index}`} className={"mt-3 dg-ion-item"}>
+                <IonLabel>
+                  <span
+                    className={
+                      "font-medium text-sm text-green-800 whitespace-pre-wrap"
+                    }
+                  >
+                    {gun.title}
+                  </span>
+                </IonLabel>
+                <IonLabel slot={"end"}>
+                  <span
+                    className={
+                      "font-medium text-sm float-right text-green-800 whitespace-pre-wrap"
+                    }
+                  >
+                    {gun.date}
+                  </span>
+                </IonLabel>
+              </IonItem>
+            ))}
+          {activeTab == "2026" &&
+            ozelGunler_2026.map((gun, index) => (
+              <IonItem key={`2026-${index}`} className={"mt-3 dg-ion-item"}>
+                <IonLabel>
+                  <span
+                    className={
+                      "font-medium text-sm text-green-800 whitespace-pre-wrap"
+                    }
+                  >
+                    {gun.title}
+                  </span>
+                </IonLabel>
+                <IonLabel slot={"end"}>
+                  <span
+                    className={
+                      "font-medium text-sm float-right text-green-800 whitespace-pre-wrap"
+                    }
+                  >
+                    {gun.date}
+                  </span>
+                </IonLabel>
+              </IonItem>
+            ))}
+          {activeTab == "2027" &&
+            ozelGunler_2027.map((gun, index) => (
+              <IonItem key={`2027-${index}`} className={"mt-3 dg-ion-item"}>
+                <IonLabel>
+                  <span
+                    className={
+                      "font-medium text-sm text-green-800 whitespace-pre-wrap"
+                    }
+                  >
+                    {gun.title.toLocaleUpperCase("tr")}
+                  </span>
+                </IonLabel>
+                <IonLabel slot={"end"}>
+                  <span
+                    className={
+                      "font-medium text-sm float-right text-green-800 whitespace-pre-wrap"
+                    }
+                  >
+                    {gun.date}
+                  </span>
+                </IonLabel>
+              </IonItem>
+            ))}
+          {activeTab == "2028" &&
+            ozelGunler_2028.map((gun, index) => (
+              <IonItem key={`2028-${index}`} className={"mt-3 dg-ion-item"}>
+                <IonLabel>
+                  <span
+                    className={
+                      "font-medium text-sm text-green-800 whitespace-pre-wrap"
+                    }
+                  >
+                    {gun.title.toLocaleUpperCase("tr")}
+                  </span>
+                </IonLabel>
+                <IonLabel slot={"end"}>
+                  <span
+                    className={
+                      "font-medium text-sm float-right text-green-800 whitespace-pre-wrap"
+                    }
+                  >
+                    {gun.date}
+                  </span>
+                </IonLabel>
+              </IonItem>
+            ))}
+        </IonList>
+      </IonContent>
+    </>
+  );
 }
 
 export default DiniGunler;
